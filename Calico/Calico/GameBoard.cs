597dfb119e5cdef9284baa84a3691e47452cfee9
krevetka159 <dita.chabicovska@gmail.com>
2023-08-28 18:56:00 +0200

    prep

--- Calico/Calico/GameBoard.cs
+++ Calico/Calico/GameBoard.cs
@@ -9,11 +9,22 @@ namespace Calico
     internal class GameBoard
     {
         private List<List<GamePiece>> board;
+        private Random random;
 
 
         public GameBoard()
         {
             board = new List<List<GamePiece>>();
+            random = new Random();
+
+            int randId = random.Next(0, 4);
+            // TODO generování okrajů boardu podle id
+
+        }
+
+        public void PrintBoard()
+        {
+
         }
     }
 }
