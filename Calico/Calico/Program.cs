f7535ccbce33217c3e016947bd3dd183cd1715da
krevetka159 <dita.chabicovska@gmail.com>
2023-08-28 18:52:30 +0200

    init console print

--- Calico/Calico/Program.cs
+++ Calico/Calico/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Kill me");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        private static void NewGame()
+        {
+            Console.WriteLine("CALICO");
+            Console.WriteLine("");
+            Console.Write("Počet hráčů: ");
+
+            string numOfPlayers = Console.ReadLine();
+
+            if (numOfPlayers == "1") { Console.WriteLine("ANO"); }
+            else { Console.WriteLine("NE"); }
+
+        }
+        static void Main(string[] args)
+        {
+
+            NewGame();
+        }
+    }
+}
