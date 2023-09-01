96e5f8f6b153bc4ddc04265329805849f683ff38
krevetka159 <dita.chabicovska@gmail.com>
2023-09-01 15:21:09 +0200

    typ

--- Calico/Calico/GamePiece.cs
+++ Calico/Calico/GamePiece.cs
@@ -11,11 +11,17 @@ namespace Calico
         public int Color;
         public int Pattern;
         private int ID;
+        public int Type;
 
         public GamePiece(int color, int pattern)
         {
             Color = color;
             Pattern = pattern;
+
+            if (pattern == 0 && color == 0) Type = 0;
+            else if (pattern == -1 && color == -1) Type = -1;
+            else Type = 1;
+            // TODO check správného předání color, pattern
             //ID = id;
         }
     }
