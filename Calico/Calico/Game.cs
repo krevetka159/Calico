9b25ed7e5229f9b753c909d8d66f4768fede1e61
krevetka159 <dita.chabicovska@gmail.com>
2023-09-26 18:06:47 +0200

    multiplayer print state

--- Calico/Calico/Game.cs
+++ Calico/Calico/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,17 @@ namespace Calico
     {
         private Bag Bag;
         private Player Player;
+        private Player Agent;
         private GamePiece[] Opts = new GamePiece[3];
+        private Scoring scoring;
         //private (GamePiece, GamePiece, GamePiece) PossiblePieces;
 
-        public Game(int numOfPlayers) 
+
+
+        public Game(int mode) 
         { 
             Bag = new Bag();
+            Scoring scoring = new Scoring();
             
             for (int i = 0; i < 3; i++)
             {
@@ -23,47 +29,65 @@ namespace Calico
             }
             
 
-            if (numOfPlayers == 1) { SinglePlayer(); }
-            else { MultiplePlayers(numOfPlayers); }
+            if (mode == 1) { SinglePlayer(); }
+            else { MultiPlayer(); }
         }
 
-
+// ----------------------------------------------- SINGLEPLAYER ------------------------------------------------------------
         public void SinglePlayer()
         {
-            //Player = new Player();
-
-            Player = new RandomAgent();
+            Player = new Player(scoring);
 
             //print empty
             PrintState();
 
             for (int i = 0; i < 22; i++)
             {
-                MakeMove();
+                MakeMove(Player);
                 // update points
                 PrintState();
                 
             }
 
-            PrintStats();
+            PrintStats(Player);
         }
 
-        public void MultiplePlayers(int numOfPlayers) 
+// ----------------------------------------------- MULTIPLAYER ------------------------------------------------------------
+        public void MultiPlayer() 
         {
+            Player = new Player(scoring);
+            Agent = new RandomAgent(scoring);
+
+            //print empty
+            PrintStateMulti();
+
+            for (int i = 0; i < 22; i++)
+            {
+                MakeMove(Player);
+                // update points
+                PrintState();
+
+                MakeMove(Agent);
+                PrintStateMulti();
+
+            }
+
+            PrintStats(Player);
+            PrintStats(Agent);
         }
 
 
 // ----------------------------------------------- GET COMMAND ------------------------------------------------------------
 
 
-        private void MakeMove()
+        private void MakeMove(Player p)
         {
    
-            int next = Player.ChooseGamePiece(Opts) - 1;
+            int next = p.ChooseGamePiece(Opts) - 1;
             
-            (int row, int col) = Player.ChoosePosition();
+            (int row, int col) = p.ChoosePosition();
 
-            Player.MakeMove(Opts[next], row-1, col-1);
+            p.MakeMove(Opts[next], row-1, col-1);
 
             Opts[next] = Bag.Next();
         }
@@ -91,12 +115,63 @@ namespace Calico
             Console.WriteLine();
         }
 
+        private void PrintStateMulti()
+        {
+            Console.WriteLine();
+
+            Console.Write("Dílky k použití: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($" {i + 1}: |{Opts[i].Print}| ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine();
+
+            // přehled kočiček
+
+            //Console.WriteLine("Hráč skóre: " + Player.board._scoreCounter.GetScore());
+            //Player.board.PrintBoard();
+            //Console.WriteLine("Agent skóre: " + Player.board._scoreCounter.GetScore());
+            //Agent.board.PrintBoard();
+
+           
+            Console.WriteLine("      1    2    3    4    5    6    7" + "        " + "      1    2    3    4    5    6    7");
+            Console.WriteLine("   ------------------------------------" + "        " + "   ------------------------------------");
+            for (int i = 0; i < Player.board.size; i++)
+            {
+
+                Console.Write(i + 1);
+                if (i % 2 == 0) Console.Write("  ");
+                Console.Write(" |");
+                for (int j = 0; j < Player.board.size; j++)
+                {
+                    GamePiece p = Player.board.board[i][j];
+                    Console.Write($"{p.Print}|");
+                }
+                Console.Write("        ");
+                Console.Write(i + 1);
+                if (i % 2 == 0) Console.Write("  ");
+                Console.Write(" |");
+                for (int j = 0; j < Agent.board.size; j++)
+                {
+                    GamePiece p = Agent.board.board[i][j];
+                    Console.Write($"{p.Print}|");
+                }
+                Console.Write("\n");
+                Console.WriteLine("   ------------------------------------" + "        " + "   ------------------------------------");
+            }
+
+            
+            Console.WriteLine();
+        }
 
 
-        private void PrintStats()
+
+        private void PrintStats(Player p)
         {
             // finální výsledky
-            Console.WriteLine("Finální skóre: " + Player.board._scoreCounter.GetScore());
+            Console.WriteLine("Finální skóre: " + p.board._scoreCounter.GetScore());
         }
     }
 }
