1762a806efffff30a1710f4482f1ef382e95699d
krevetka159 <dita.chabicovska@gmail.com>
2023-09-29 11:48:44 +0200

    documentation

--- Calico/Calico/ScoreCounter.cs
+++ Calico/Calico/ScoreCounter.cs
@@ -26,12 +26,21 @@ namespace Calico
 
         }
 
+        /// <summary>
+        /// Adds patchtile to color and pattern UnionFinds
+        /// </summary>
+        /// <param name="piece"></param>
         public void AddToUF(GamePiece piece)
         {
             patternUF.Add(piece);
             colorUF.Add(piece);
         }
 
+        /// <summary>
+        /// Checks if 2 patchtiles have the same color/pattern and unites them in UnionFind if needed
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="n"></param>
         public void EvaluateNew(GamePiece p, GamePiece n)
         {
             if (p.Color == n.Color)
@@ -64,48 +73,94 @@ namespace Calico
             }
 
         }
-
+        /// <summary>
+        /// Checks whether two patchtiles are in the same color cluster
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public bool CheckColorUnion(GamePiece p, GamePiece n)
         {
             return colorUF.Find(p, n);
         }
 
+        /// <summary>
+        /// Checks whether two patchtiles are in the same color cluster
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public bool CheckPatternUnion(GamePiece p, GamePiece n)
         {
             return patternUF.Find(p, n);
         }
 
+
+        /// <summary>
+        /// Returns size of color cluster that the patchtile is a part of
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public int GetColorCount(GamePiece p)
         {
             return colorUF.Count(p);
         }
 
+        /// <summary>
+        /// Returns scoring of color cluster that the patchtile is a part of
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public int GetColorScore(GamePiece p)
         {
             return (colorUF.Count(p) /scoring.ColorClusterSize) * scoring.ColorClusterScore;
         }
 
+        /// <summary>
+        /// Returns size of pattern cluster that the patchtile is a part of
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public int GetPatternCount(GamePiece p)
         {
             return patternUF.Count(p);
         }
 
+        /// <summary>
+        /// Returns scoring of pattern cluster that the patchtile is a part of
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
         public int GetPatternScore(GamePiece p)
         {
             return (patternUF.Count(p) / scoring.PatternClusterSizes[p.Pattern]) * scoring.PatternClusterScores[p.Pattern];
         }
 
+        /// <summary>
+        /// Count color score from the number of tiles in a cluster
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public int CountColorScore(int count)
         {
             return (count / scoring.ColorClusterSize) * scoring.ColorClusterScore;
         }
 
+        /// <summary>
+        /// Count pattern score from the number of tiles in a cluster
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public int CountPatternScore(int count, Pattern p) 
         {
             return (count / scoring.PatternClusterSizes[p]) * scoring.PatternClusterScores[p];
         }
 
 
+        /// <summary>
+        /// Returns the score value
+        /// </summary>
+        /// <returns></returns>
         public int GetScore()
         {
             return Score;
