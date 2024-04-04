ebe17777f8e92c9892be78471c85c5144f7c9450
krevetka159 <dita.chabicovska@gmail.com>
2024-04-04 14:04:28 +0200

    rozdělelní scoring na víc tříd

--- Calico/Calico/Scoring.cs
+++ Calico/Calico/Scoring.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Calico
 {
@@ -16,7 +18,11 @@ namespace Calico
         public int ColorClusterSize = 3;
         public Dictionary<Pattern, int> PatternClusterSizes { get; private set; }
 
-        public StringBuilder PatternScoring { get; private set; }
+        public ColorScoring ColorScoring { get; private set; }
+
+        public PatternScoring PatternScoring {  get; private set; }
+
+        public StringBuilder PatternScoringToString { get; private set; }
         // for printState in game
 
         public Scoring() 
@@ -40,27 +46,139 @@ namespace Calico
             PatternClusterScores = new Dictionary<Pattern, int>();
             PatternClusterSizes = new Dictionary<Pattern, int>();
 
-            PatternScoring = new StringBuilder();
+            PatternScoringToString = new StringBuilder();
 
             foreach ((int size, int score) in patternProps)
             {
-                PatternScoring.Append($" {size} patchtiles = {score} points for patterns ");
+                PatternScoringToString.Append($" {size} patchtiles = {score} points for patterns ");
                 for (int i = 0; i < 2; i++)
                 {
                     int randInt = random.Next(patterns.Count);
                     PatternClusterSizes[patterns[randInt]] = size;
                     PatternClusterScores[patterns[randInt]] = score;
 
-                    PatternScoring.Append((char)(64+(int)patterns[randInt]));
-                    PatternScoring.Append(", ");
+                    PatternScoringToString.Append((char)(64+(int)patterns[randInt]));
+                    PatternScoringToString.Append(", ");
                     
                     patterns.RemoveAt(randInt);
                 }
-                PatternScoring.Append("\n");
+                PatternScoringToString.Append("\n");
             }
 
 
+            PatternScoring = new PatternScoring();
+            ColorScoring = new ColorScoring();
+        }
+    }
+    public class PatternScoring
+    {
+        private Random random;
+        public Dictionary<Pattern, PatternScoringPanel> PatternScoringDict {  get; private set; }
+        public List<PatternScoringPanel> PatternScoringPanels { get; private set; }
 
+        public StringBuilder PatternScoringPrint {  get; private set; }
+
+        public PatternScoring()
+        {
+            random = new Random();
+
+            List<Pattern> patterns = new List<Pattern>();
+
+            foreach (Pattern i in Enum.GetValues(typeof(Pattern)))
+            {
+                if (i != Pattern.None)
+                {
+                    patterns.Add(i);
+                }
+            }
+
+            PatternScoringPrint = new StringBuilder();
+
+            PatternScoringPanels = new List<PatternScoringPanel>();
+            PatternScoringDict = new Dictionary<Pattern, PatternScoringPanel>();
+
+            //foreach ((int size, int score) in patternProps)
+            //{
+            //    PatternScoringToString.Append($" {size} patchtiles = {score} points for patterns ");
+            //    for (int i = 0; i < 2; i++)
+            //    {
+            //        int randInt = random.Next(patterns.Count);
+            //        PatternClusterSizes[patterns[randInt]] = size;
+            //        PatternClusterScores[patterns[randInt]] = score;
+
+            //        PatternScoringToString.Append((char)(64 + (int)patterns[randInt]));
+            //        PatternScoringToString.Append(", ");
+
+            //        patterns.RemoveAt(randInt);
+            //    }
+            //    PatternScoringToString.Append("\n");
+
+            //}
+
+            for (int i = 1; i <= 3; i++)
+            {
+                int randInt = random.Next(patterns.Count);
+                Pattern p1 = patterns[randInt];
+                patterns.RemoveAt(randInt);
+
+                randInt = random.Next(patterns.Count);
+                Pattern p2 = patterns[randInt];
+                patterns.RemoveAt(randInt);
+
+                PatternScoringPanel panel = new PatternScoringPanel(i, p1, p2);
+                PatternScoringPanels.Add(panel);
+                PatternScoringDict[p1] = panel;
+                PatternScoringDict[p2] = panel;
+            }
+
+            foreach ( PatternScoringPanel panel in PatternScoringPanels)
+            {
+                PatternScoringPrint.Append($" {panel.ClusterSize} patchtiles = {panel.Points} points for patterns {panel.PatternsPrint} \n");
+            }
+
+        }
+    }
+
+    public class PatternScoringPanel
+    {
+        public int Points;
+        public int ClusterSize;
+        public (Pattern, Pattern) Patterns;
+        public string PatternsPrint;
+
+        public PatternScoringPanel(int id, Pattern p1, Pattern p2)
+        {
+            Patterns = (p1,p2);
+            switch (id)
+            {
+                case 1:
+                    Points = 3;
+                    ClusterSize = 3;
+                    break;
+                case 2:
+                    Points = 5;
+                    ClusterSize = 4;
+                    break;
+                case 3:
+                    Points = 7;
+                    ClusterSize = 5;
+                    break;
+            }
+
+            PatternsPrint = $"{(char)(64 + (int)p1)}, {(char)(64 + (int)p2)}";
+
+        }
+    }
+
+    public class ColorScoring
+    {
+        public int Points;
+        public int ClusterSize;
+
+        public ColorScoring()
+        {
+            Points = 3;
+            ClusterSize = 3;
         }
     }
 }
