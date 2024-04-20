db8e7ce4d24ae5a81b95f95be1380bbe51aa4524
krevetka159 <dita.chabicovska@gmail.com>
2024-04-20 22:37:31 +0200

    setting testing

--- Calico/Calico/GameModeController.cs
+++ Calico/Calico/GameModeController.cs
@@ -57,6 +57,7 @@ namespace Calico
                     {
                         //TestTasks();
                         //TestBoards();
+                        TestAllSettings();
                         break;
                     }
                 case 7:
@@ -283,9 +284,118 @@ namespace Calico
 
         #endregion
 
-        #region Task Analysis
+        #region Settings analysis
 
-        private AverageGameStats TestTask(int agentType, int iterations, (int, int, int) tasks)
+        private AverageGameStats TestSetting(int agentType, int iterations, (int, int, int) tasks, int boardId)
+        {
+            List<GameStats> stats = new List<GameStats>();
+
+            for (int j = 0; j < iterations; j++)
+            {
+                stats.Add(null);
+            }
+
+            Parallel.For(0, iterations, j =>
+            {
+                Game g = new Game();
+                g.AgentGameSettings(7, false, tasks, boardId);
+                stats[j] = g.Stats;
+            });
+
+            if (iterations > 1)
+            {
+                Console.WriteLine(" Mean: " + stats.Average(item => item.Score));
+                //Console.WriteLine(" Average number of buttons: " + (Convert.ToDouble(buttons) / Convert.ToDouble(iterations)));
+                //Console.WriteLine(" Average number of cats: " + (Convert.ToDouble(cats.Item1) / Convert.ToDouble(iterations)) + "; " + (Convert.ToDouble(cats.Item2) / Convert.ToDouble(iterations)) + "; " + (Convert.ToDouble(cats.Item3) / Convert.ToDouble(iterations)));
+                //Console.WriteLine(" Best score: " + max + "; buttons: " + Convert.ToDouble(maxButtons) +
+                //    "; cats: " + Convert.ToDouble(maxCats.Item1) + "; " + Convert.ToDouble(maxCats.Item2) + "; " + Convert.ToDouble(maxCats.Item3));
+                //Console.WriteLine(" Lowest score: " + min);
+            }
+
+            return new AverageGameStats(agentType, stats.Average(item => item.Score), stats.Average(item => item.Buttons), (stats.Average(item => item.Cats.Item1), stats.Average(item => item.Cats.Item2), stats.Average(item => item.Cats.Item3)), stats.Max(item => item.Score), stats.Min(item => item.Score));
+
+        }
+
+        private void TestAllSettings()
+        {
+            List<Dictionary<(int, int, int), List<AverageGameStats>>> statsDict = new List<Dictionary<(int, int, int), List<AverageGameStats>>>();
+
+            for (int b =  0; b < 4; b++)
+            {
+                statsDict.Add(new Dictionary<(int, int, int), List<AverageGameStats>>());
+                for (int i = 1; i <= 6; i++)
+                {
+                    for (int j = i + 1; j <= 6; j++)
+                    {
+                        for (int k = j + 1; k <= 6; k++)
+                        {
+                            
+                            statsDict[b][(i, j, k)] = new List<AverageGameStats>();
+
+                            Console.WriteLine($" {b} - {i},{j},{k}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (i, j, k), b));
+                            Console.WriteLine();
+
+                            Console.WriteLine($" {b} - {i},{k},{j}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (i, k, j), b));
+                            Console.WriteLine();
+
+                            Console.WriteLine($" {b} - {j},{i},{k}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (j, i, k), b));
+                            Console.WriteLine();
+
+                            Console.WriteLine($" {b} - {j},{k},{i}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (j, k, i), b));
+                            Console.WriteLine();
+
+                            Console.WriteLine($" {b} - {k},{i},{j}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (k, i, j), b));
+                            Console.WriteLine();
+
+                            Console.WriteLine($" {b} - {k},{j},{i}: ");
+                            statsDict[b][(i, j, k)].Add(TestSetting(7, 5000, (k, j, i), b));
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                List<(string, List<double>)> avgScores = new List<(string, List<double>)>();
+                foreach ((int, int, int) tasks in statsDict[0].Keys)
+                {
+                    avgScores.Add(($"{tasks.Item1},{tasks.Item2},{tasks.Item3}",
+                        new List<double>() { 
+                            statsDict[0][tasks].Average(item => item.AvgScore),
+                            statsDict[1][tasks].Average(item => item.AvgScore),
+                            statsDict[2][tasks].Average(item => item.AvgScore),
+                            statsDict[3][tasks].Average(item => item.AvgScore),
+                        }));
+                }
+                avgScores.Sort((x, y) => x.Item2.Average().CompareTo(y.Item2.Average()));
+
+                using (StreamWriter outputFile = new StreamWriter($"./testSetting.csv"))
+                {
+                    outputFile.WriteLine("tasks;avg1;avg2;avg3;avg4;avg");
+                    foreach ((string, List<double>) ds in avgScores)
+                    {
+                        outputFile.WriteLine($"{ds.Item1};" +
+                            $"{Math.Round(ds.Item2[0], 3, MidpointRounding.AwayFromZero).ToString("0.000")};" +
+                            $"{Math.Round(ds.Item2[1], 3, MidpointRounding.AwayFromZero).ToString("0.000")};" +
+                            $"{Math.Round(ds.Item2[2], 3, MidpointRounding.AwayFromZero).ToString("0.000")};" +
+                            $"{Math.Round(ds.Item2[3], 3, MidpointRounding.AwayFromZero).ToString("0.000")};" +
+                            $"{Math.Round(ds.Item2.Average(), 3, MidpointRounding.AwayFromZero).ToString("0.000")}");
+                    }
+                }
+            }
+            
+        }
+
+
+            #region Task Analysis
+
+            private AverageGameStats TestTask(int agentType, int iterations, (int, int, int) tasks)
         {
             List<GameStats> stats = new List<GameStats>();
 
@@ -515,6 +625,10 @@ namespace Calico
             }
         }
         #endregion
+
+        #endregion
+
+
 
 
         #region Evolution
