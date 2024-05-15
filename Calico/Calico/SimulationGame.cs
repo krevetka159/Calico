bb2ac985af9daec1997c6ea7fb0967ca2643ce69
krevetka159 <dita.chabicovska@gmail.com>
2024-05-15 11:49:16 +0200

    simulace hry

---
+++ Calico/Calico/SimulationGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calico
+{
+    public class SimulationGame : Game
+    {
+        public SimulationGame(GameBoard gb, GamePiece[] o, Scoring s)
+        {
+            bag = new Bag();
+            for (int i = 0; i < o.Length; i++)
+            {
+                Opts[i] = o[i];
+            }
+            for (int i = o.Length; i < 3; i++)
+            {
+                Opts[i] = bag.Next();
+            }
+
+            scoring = s;
+
+            agent = UseAgent(7);
+            agent.Board = gb;
+
+
+        }
+
+        public int Game()
+        {
+            for (int i = 0; i < agent.Board.EmptySpotsCount; i++)
+            {
+                MakeMove(agent);
+            }
+
+            return agent.Board.ScoreCounter.Score;
+        }
+    }
+}
